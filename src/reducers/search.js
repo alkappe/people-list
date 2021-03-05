a46a37b5b3bc49dc815f85eab54224184423ed6d
alkappe <alka.cappellazzo@gmail.com>
2021-03-05 14:16:42 +0100

    commented console.log

--- src/reducers/search.js
+++ src/reducers/search.js
@@ -1,5 +1,5 @@
 const search = (state = '', action) => {
-    console.log(action)
+    /* console.log(action) */
     //modalita switch dato uno stato iniziale e l'azione iniziale
     switch(action.type) {
         case 'SEARCH':
